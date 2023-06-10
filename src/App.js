4821bee2909281929fbc7b9b26da93d35c9c7bf9
Riririririii <127101360+Riririririii@users.noreply.github.com>
2023-06-10 11:02:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import Home from "./client/Home/Home"
-import Login from "./client/Login/Login"
-import Signup from "./client/Signup/Signup";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import React from 'react';
+import { SideBar } from "./client/Components/Sidebar";
+import './client/Form.css';
+import { Grid } from 'semantic-ui-react';
 
 function App() {
-  const { currentUser } = useContext(AuthContext);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to="/login" />;
-    }
-
-    return children
-  };
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Grid columns="equal">
+      <SideBar />
+      <Grid.Column className="messagepanel">
+        
+      </Grid.Column>
+
+      <Grid.Column width={3}>
+        <span>
+
+        </span>
+      </Grid.Column>
+    </Grid>
+
   );
 }
 
