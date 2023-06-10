08076f70d95d4d1847b63a33bdc39fd0272dfc9c
Riririririii <127101360+Riririririii@users.noreply.github.com>
2023-06-10 11:02:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthContextProvider } from "./context/AuthContext";
-import { ChatContextProvider } from "./context/ChatContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Login from './client/Login/Login';
+import Signup from './client/Signup/Signup';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { onAuthStateChanged } from 'firebase/auth';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthContextProvider>
-    <ChatContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChatContextProvider>
-  </AuthContextProvider>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
