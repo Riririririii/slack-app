b6b97db0b00df70b77d36f0a30e3e01e1a409ba5
Riririririii <127101360+Riririririii@users.noreply.github.com>
2023-06-10 11:02:53 +0800

    Update Signup.jsx

--- src/client/Signup/Signup.jsx
+++ src/client/Signup/Signup.jsx
@@ -1,88 +1,194 @@
 import React, { useState } from "react";
-import Add from "../../img/blank.jpg";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, db, storage } from "../../server/firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore";
-import { useNavigate, Link } from "react-router-dom";
+import { Grid, Segment, Form, Icon, Header, Button, Message } from "semantic-ui-react";
+import "../Form.css";
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth } from "../../server/firebase";
+import { addDoc, collection } from "@firebase/firestore";
+import { db } from "../../server/firebase";
+import { Link, useNavigate } from "react-router-dom";
 
-const Register = () => {
-  const [err, setErr] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    setLoading(true);
-    e.preventDefault();
-    const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const file = e.target[3].files[0];
+const Signup = () => {
 
-    try {
-      //Create user
-      const res = await createUserWithEmailAndPassword(auth, email, password);
+    const [userState, setUserState] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
 
-      //Create a unique image name
-      const date = new Date().getTime();
-      const storageRef = ref(storage, `${displayName + date}`);
+    const [errorState, setErrorState] = useState([]);
+    const [successState, setSuccessState] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
-      await uploadBytesResumable(storageRef, file).then(() => {
-        getDownloadURL(storageRef).then(async (downloadURL) => {
-          try {
-            //Update profile
-            await updateProfile(res.user, {
-              displayName,
-              photoURL: downloadURL,
-            });
-            //create user on firestore
-            await setDoc(doc(db, "users", res.user.uid), {
-              uid: res.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
+    const handleInput = (event) => {
+        const { name, value } = event.target;
+        setUserState((prevState) => ({
+        ...prevState,
+        [name]: value
+        }));
+        };
 
-            //create empty user chats on firestore
-            await setDoc(doc(db, "userChats", res.user.uid), {});
-            navigate("/");
-          } catch (err) {
-            console.log(err);
-            setErr(true);
-            setLoading(false);
-          }
-        });
-      });
-    } catch (err) {
-      setErr(true);
-      setLoading(false);
-    }
-  };
+    const checkForm = () => {
 
-  return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
-        <span className="title">Register</span>
-        <form onSubmit={handleSubmit}>
-          <input required type="text" placeholder="display name" />
-          <input required type="email" placeholder="email" />
-          <input required type="password" placeholder="password" />
-          <input required style={{ display: "none" }} type="file" id="file" />
-          <label htmlFor="file">
-            <img src={Add} alt="" />
-            <span>Add an avatar</span>
-          </label>
-          <button disabled={loading}>Sign up</button>
-          {loading && "Uploading and compressing the image please wait..."}
-          {err && <span>Something went wrong</span>}
-        </form>
-        <p>
-          You do have an account? <Link to="/register">Login</Link>
-        </p>
-      </div>
-    </div>
-  );
-};
+            if (isFormEmpty()) {
+                setErrorState((prevState) => [...prevState, { message: "Please fill in all fields" }]);
+                return false;
+            } else if (!checkPassword()) {
+                return false;
+            }
+            return true;
+    };
 
-export default Register;
+    const isFormEmpty = () => {
+        const { userName, email, password, confirmPassword } = userState;
+        return !userName.length || !email.length || !password.length || !confirmPassword.length;
+    };
+
+    const checkPassword = () => {
+        const { password, confirmPassword } = userState;
+
+            if (password.length < 8) {
+                setErrorState((prevState) => [...prevState, { message: "Password length should be greater than 8" }]);
+                return false;
+            } else if (password !== confirmPassword) {
+                setErrorState((prevState) => [...prevState, { message: "Password and Confirm Password does not match" }]);
+                return false;
+            } else 
+                return true;
+
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setErrorState(() => []);
+        setSuccessState(() => []);
+
+            if (checkForm()) {
+                setIsLoading(true);
+                await createUserWithEmailAndPassword(auth, userState.email, userState.password)
+                .then((userCredential) => {
+                    setIsLoading(false);
+                    updateUser(userCredential); 
+                })
+                .catch(servererror => {
+                    setIsLoading(false);
+                    setErrorState((prevState) => [...prevState, servererror]);
+                })  
+            } else {
+            
+            }
+
+    };
+
+    const updateUser = async (userCredential) => {
+
+        const add = collection(db, "userInfo")
+        const data = {
+            displayName: userState.userName,
+            photoURL: `http://gravatar.com/avatar/${userCredential.user.uid}?d=identicon`
+        }
+
+            if (userCredential) {
+                setIsLoading(true);
+                    try {
+                    await updateProfile(userCredential.user, data);
+                        setIsLoading(false);
+                        console.log("Created Successfully");
+                    } catch (serverError) {
+                        setIsLoading(false);
+                        setErrorState((prevState) => [...prevState, serverError]);
+                    }
+                    addDoc(add, data); 
+                    setSuccessState((prevState) => [...prevState, { message: "Account Created Successfully" }]);   
+                    setIsLoading(false);
+                    navigate("/login");
+                                 
+            }
+    
+    }   
+
+    const formatError = () => {
+        return errorState.map((error, index) => <p key={index}>{error.message}</p>);
+    };
+
+    const formatSuccess = () => {
+        return successState.map((success, index) => <p key={index}>{success.message}</p>);
+    };
+    
+
+    return (
+        <>
+        <Grid verticalAlign="middle" textAlign="center" className="grid-form">
+            <Grid.Column style={{ maxWidth: "500px" }}>
+            <Header icon as="h2">
+                <Icon name="slack" />
+                Register
+            </Header>
+            <Form onSubmit={handleSubmit}>
+                <Segment stacked>
+                <Form.Input
+                    name="userName"
+                    value={userState.userName}
+                    icon="user"
+                    iconPosition="left"
+                    onChange={handleInput}
+                    type="text"
+                    placeholder="Username"
+                />
+                <Form.Input
+                    name="email"
+                    value={userState.email}
+                    icon="mail"
+                    iconPosition="left"
+                    onChange={handleInput}
+                    type="text"
+                    placeholder="Email"
+                />
+                <Form.Input
+                    name="password"
+                    value={userState.password}
+                    icon="lock"
+                    iconPosition="left"
+                    onChange={handleInput}
+                    type="password"
+                    placeholder="Password"
+                />
+                <Form.Input
+                    name="confirmPassword"
+                    value={userState.confirmPassword}
+                    icon="lock"
+                    iconPosition="left"
+                    onChange={handleInput}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <Button disabled={isLoading} loading={isLoading}>Submit</Button>
+                </Segment>
+                
+            </Form>
+            {errorState.length > 0 && (
+                <Message error>
+                <h4>Error</h4>
+                {formatError()}
+                </Message>
+                
+            )}
+            {successState.length > 0 && (
+                <Message success>
+                <h4>Success</h4>
+                {formatSuccess()}
+                </Message>
+                
+            )}
+            <Message>
+                Already an user? <Link to="/login">Login</Link>
+            </Message>
+            </Grid.Column>
+        </Grid>
+        </>
+    );
+    };
+
+    export default Signup;
